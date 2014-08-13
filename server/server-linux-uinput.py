844d0edfe182b0e62272c037e1caeb38ad7a8c3a
Nils Berg <berg.nils@gmail.com>
2014-08-13 23:14:58 +0200

    experimental linux variant using uinput (install it using pip!)

---
+++ server/server-linux-uinput.py
@@ -0,0 +1,24 @@
+
+import libi2dx
+import uinput
+
+
+class I2DXWebSocketUInput(libi2dx.I2DXWebSocket):
+    keys = []
+    for player in range(1, 5):
+        if libi2dx.config.has_section('keymap_player%s' % player):
+            for x in libi2dx.config.items('keymap_player%s' % player):
+                keys.append(uinput._CHAR_MAP.get(x[1]))
+
+    device = uinput.Device(keys)
+
+    def toggle_key(self, key_id, active, player):
+        if active:
+            active = 1
+        else:
+            active = 0
+        self.device.emit(uinput._CHAR_MAP.get(
+            libi2dx.config.get('keymap_player%s' % player, key_id)), active)
+
+if __name__ == "__main__":
+    libi2dx.serve(I2DXWebSocketUInput)
