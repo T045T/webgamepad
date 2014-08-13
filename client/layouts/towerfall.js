25c787361ebd99ce82eae18f4aa1a9a4e802cc54
Nils Berg <berg.nils@gmail.com>
2014-08-14 00:46:47 +0200

    add towerfall skin

---
+++ client/layouts/towerfall.js
@@ -0,0 +1,59 @@
+
+i2DX.layout('towerfall', function(ui) {
+
+    ui.elementButton('towerfall_up', {
+	left: '30vmin',
+	bottom: '46vmin',
+	width: '16vmin',
+	height: '25vmin',
+    });
+
+    ui.elementButton('towerfall_down', {
+	left: '30vmin',
+	bottom: '5vmin',
+	width: '16vmin',
+	height: '25vmin',
+    });
+
+    ui.elementButton('towerfall_left', {
+	left: '5vmin',
+	bottom: '30vmin',
+	width: '25vmin',
+	height: '16vmin',
+    });
+
+    ui.elementButton('towerfall_right', {
+	left: '46vmin',
+	bottom: '30vmin',
+	width: '25vmin',
+	height: '16vmin',
+    });
+
+    ui.elementButton('towerfall_jump', {
+	right: '30vmin',
+	bottom: '5vmin',
+	width: '20vmin',
+	height: '20vmin',
+    });
+
+    ui.elementButton('towerfall_shoot', {
+	right: '5vmin',
+	bottom: '25vmin',
+	width: '20vmin',
+	height: '20vmin',
+    });
+
+    ui.elementButton('towerfall_dash1', {
+	left: '0px',
+	bottom: '50vmin',
+	width: '25vmin',
+	height: '30vmin',
+    });
+
+    ui.elementButton('towerfall_dash2', {
+	right: '0px',
+	bottom: '50vmin',
+	width: '25vmin',
+	height: '30vmin',
+    });
+});
