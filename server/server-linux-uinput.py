366e51da241069806d71d10cb3893e5c1735e45e
Nils Berg <berg.nils@gmail.com>
2014-08-13 23:29:08 +0200

    prevent weird values getting into key list when a key doesn't exist in the uinput map

--- server/server-linux-uinput.py
+++ server/server-linux-uinput.py
@@ -8,17 +8,16 @@ class I2DXWebSocketUInput(libi2dx.I2DXWebSocket):
     for player in range(1, 5):
         if libi2dx.config.has_section('keymap_player%s' % player):
             for x in libi2dx.config.items('keymap_player%s' % player):
-                keys.append(uinput._CHAR_MAP.get(x[1]))
+                keys.append(uinput._CHAR_MAP.get(x[1], uinput.KEY_BACKSLASH))
 
     device = uinput.Device(keys)
-
+    # device = uinput.Device([uinput.KEY_H])
     def toggle_key(self, key_id, active, player):
         if active:
             active = 1
         else:
             active = 0
-        self.device.emit(uinput._CHAR_MAP.get(
-            libi2dx.config.get('keymap_player%s' % player, key_id)), active)
+        self.device.emit(uinput._CHAR_MAP.get(libi2dx.config.get('keymap_player%s' % player, key_id), uinput.KEY_BACKSLASH), active)
 
 if __name__ == "__main__":
     libi2dx.serve(I2DXWebSocketUInput)
