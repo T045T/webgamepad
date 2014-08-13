babeeaea550f4fd3530408106b469ea7a52c6cda
Nils Berg <berg.nils@gmail.com>
2014-08-14 01:40:33 +0200

    each player gets their own device

--- server/server-linux-uinput.py
+++ server/server-linux-uinput.py
@@ -8,16 +8,20 @@ class I2DXWebSocketUInput(libi2dx.I2DXWebSocket):
     for player in range(1, 5):
         if libi2dx.config.has_section('keymap_player%s' % player):
             for x in libi2dx.config.items('keymap_player%s' % player):
-                keys.append(uinput._CHAR_MAP.get(x[1], uinput.KEY_BACKSLASH))
+                keyval = uinput._CHAR_MAP.get(x[1], uinput.KEY_BACKSLASH)
+                if keyval not in keys:
+                    keys.append(keyval)
 
-    device = uinput.Device(keys)
-    # device = uinput.Device([uinput.KEY_H])
+    devicemap = {}
+
     def toggle_key(self, key_id, active, player):
+        if player not in self.devicemap():
+            self.devicemap[player] = uinput.Device(self.keys)
         if active:
             active = 1
         else:
             active = 0
-        self.device.emit(uinput._CHAR_MAP.get(libi2dx.config.get('keymap_player%s' % player, key_id), uinput.KEY_BACKSLASH), active)
+        self.devicemap[player].emit(uinput._CHAR_MAP.get(libi2dx.config.get('keymap_player%s' % player, key_id), uinput.KEY_BACKSLASH), active)
 
 if __name__ == "__main__":
     libi2dx.serve(I2DXWebSocketUInput)
