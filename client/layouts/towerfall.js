a18fee27a6a029c4dbc068297ef042c76a8eaa3d
Nils Berg <berg.nils@gmail.com>
2014-08-14 05:05:59 +0200

    move buttons a little

--- client/layouts/towerfall.js
+++ client/layouts/towerfall.js
@@ -2,57 +2,57 @@
 i2DX.layout('towerfall', function(ui) {
 
     ui.elementButton('towerfall_up', {
-	left: '30vmin',
-	bottom: '46vmin',
+	left: '35vmin',
+	bottom: '51vmin',
 	width: '16vmin',
 	height: '25vmin',
     });
 
     ui.elementButton('towerfall_down', {
-	left: '30vmin',
-	bottom: '5vmin',
+	left: '35vmin',
+	bottom: '10vmin',
 	width: '16vmin',
 	height: '25vmin',
     });
 
     ui.elementButton('towerfall_left', {
-	left: '5vmin',
-	bottom: '30vmin',
+	left: '10vmin',
+	bottom: '35vmin',
 	width: '25vmin',
 	height: '16vmin',
     });
 
     ui.elementButton('towerfall_right', {
-	left: '46vmin',
-	bottom: '30vmin',
+	left: '51vmin',
+	bottom: '35vmin',
 	width: '25vmin',
 	height: '16vmin',
     });
 
     ui.elementButton('towerfall_jump', {
-	right: '30vmin',
-	bottom: '5vmin',
+	right: '35vmin',
+	bottom: '10vmin',
 	width: '20vmin',
 	height: '20vmin',
     });
 
     ui.elementButton('towerfall_shoot', {
-	right: '5vmin',
-	bottom: '25vmin',
+	right: '10vmin',
+	bottom: '30vmin',
 	width: '20vmin',
 	height: '20vmin',
     });
 
     ui.elementButton('towerfall_dash1', {
 	left: '0px',
-	bottom: '50vmin',
+	bottom: '60vmin',
 	width: '25vmin',
 	height: '30vmin',
     });
 
     ui.elementButton('towerfall_dash2', {
 	right: '0px',
-	bottom: '50vmin',
+	bottom: '60vmin',
 	width: '25vmin',
 	height: '30vmin',
     });
