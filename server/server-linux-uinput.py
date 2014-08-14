abc05b36eab04b9eaaf4a43bd47a2bc68a286c2c
Nils Berg <berg.nils@gmail.com>
2014-08-14 16:12:09 +0200

    have server-linux-uinput spawn xboxdrv itself

--- server/server-linux-uinput.py
+++ server/server-linux-uinput.py
@@ -1,9 +1,18 @@
 
 import libi2dx
 import uinput
-
+import difflib
+import subprocess
+import time
+import shlex
+import sys
+import signal
+import os
 
 class I2DXWebSocketUInput(libi2dx.I2DXWebSocket):
+    extra_args = " ".join(sys.argv[1:])
+    print extra_args
+
     keys = []
     for player in range(1, 5):
         if libi2dx.config.has_section('keymap_player%s' % player):
@@ -13,14 +22,25 @@ class I2DXWebSocketUInput(libi2dx.I2DXWebSocket):
                     keys.append(keyval)
 
     devicemap = {}
-
+    xboxmap = {}
+    
     def toggle_key(self, key_id, active, player):
         if player not in self.devicemap:
+            old_ls = subprocess.check_output('ls /dev/input/', shell=True).split()
             self.devicemap[player] = uinput.Device(self.keys,
                                                    'web_kbd%s' % player,
-                                                   0x01,  # BUS_USB from linux/input.h
+                                                   0x03,  # BUS_USB (at least on Ubuntu...)
                                                    0x1337,  # fake vendor id
                                                    0x1337)  # fake device id
+            time.sleep(1)
+            new_ls = subprocess.check_output('ls /dev/input/', shell=True).split()
+            matcher = difflib.SequenceMatcher(None, old_ls, new_ls)
+            for tag, i1, i2, j1, j2 in reversed(matcher.get_opcodes()):
+                if tag == 'insert':
+                    print "new device id is %s" % new_ls[j1]
+                    print subprocess.check_output('pwd', shell=True)
+                    args = shlex.split("xboxdrv -c " + os.path.abspath(os.path.join(os.path.dirname(__file__), '../web2xbox.xbox')) + ' --evdev /dev/input/' + new_ls[j1]+ " " + self.extra_args)
+                    self.xboxmap[player] = subprocess.Popen(args)
         if active:
             active = 1
         else:
