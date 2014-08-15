378ad5d86ff844e9d929462463360592926772e1
Nils Berg <berg.nils@gmail.com>
2014-08-15 03:48:15 +0200

    emulate most xbox controller buttons - triggers and analog sticks are missing, but all the others are there

--- server/server-linux-uinput.py
+++ server/server-linux-uinput.py
@@ -10,16 +10,26 @@ import signal
 import os
 
 class I2DXWebSocketUInput(libi2dx.I2DXWebSocket):
+    btn_map = {
+        "xbox_up": uinput.BTN_0,
+        "xbox_down": uinput.BTN_1,
+        "xbox_left": uinput.BTN_2,
+        "xbox_right": uinput.BTN_3,
+        "xbox_guide": uinput.BTN_4,
+        "xbox_A": uinput.BTN_A,
+        "xbox_B": uinput.BTN_B,
+        "xbox_X": uinput.BTN_X,
+        "xbox_Y": uinput.BTN_Y,
+        "xbox_LB": uinput.BTN_TL,
+        "xbox_RB": uinput.BTN_TR,
+        "xbox_start": uinput.BTN_START,
+        "xbox_select": uinput.BTN_SELECT
+        }
+    
     extra_args = " ".join(sys.argv[1:])
     print extra_args
 
-    keys = []
-    for player in range(1, 5):
-        if libi2dx.config.has_section('keymap_player%s' % player):
-            for x in libi2dx.config.items('keymap_player%s' % player):
-                keyval = uinput._CHAR_MAP.get(x[1], uinput.KEY_BACKSLASH)
-                if keyval not in keys:
-                    keys.append(keyval)
+    keys = btn_map.values()
 
     devicemap = {}
     xboxmap = {}
@@ -28,24 +38,28 @@ class I2DXWebSocketUInput(libi2dx.I2DXWebSocket):
         if player not in self.devicemap:
             old_ls = subprocess.check_output('ls /dev/input/', shell=True).split()
             self.devicemap[player] = uinput.Device(self.keys,
-                                                   'web_kbd%s' % player,
+                                                   'web_gamepad%s' % player,
                                                    0x03,  # BUS_USB (at least on Ubuntu...)
                                                    0x1337,  # fake vendor id
                                                    0x1337)  # fake device id
-            time.sleep(1)
+            time.sleep(1) # give uinput time to make device...
             new_ls = subprocess.check_output('ls /dev/input/', shell=True).split()
             matcher = difflib.SequenceMatcher(None, old_ls, new_ls)
             for tag, i1, i2, j1, j2 in reversed(matcher.get_opcodes()):
                 if tag == 'insert':
-                    print "new device id is %s" % new_ls[j1]
-                    print subprocess.check_output('pwd', shell=True)
-                    args = shlex.split("xboxdrv -c " + os.path.abspath(os.path.join(os.path.dirname(__file__), '../web2xbox.xbox')) + ' --evdev /dev/input/' + new_ls[j1]+ " " + self.extra_args)
+                    devname = new_ls[j1]
+                    print "new device id is %s" % devname
+                    if not devname.startswith('event'):
+                        print "not an event device, ignoring"
+                        continue
+                    command = "xboxdrv -c " + os.path.abspath(os.path.join(os.path.dirname(__file__), '../web2xbox.xbox')) + ' --evdev /dev/input/' + devname + " " + self.extra_args
+                    args = shlex.split(command)
                     self.xboxmap[player] = subprocess.Popen(args)
         if active:
             active = 1
         else:
             active = 0
-        self.devicemap[player].emit(uinput._CHAR_MAP.get(libi2dx.config.get('keymap_player%s' % player, key_id), uinput.KEY_BACKSLASH), active)
+        self.devicemap[player].emit(self.btn_map[key_id], active)
 
 if __name__ == "__main__":
     libi2dx.serve(I2DXWebSocketUInput)
